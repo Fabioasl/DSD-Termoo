b30f0e0d89a9fd39ed416845eb34adb121adccbe
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2025-06-26 18:46:05 +0000

    feat: Cria o projeto Termo com grpc

---
+++ termoo/cliente.js
@@ -0,0 +1,54 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const readline = require('readline-sync');
+const util = require('util');
+
+const CAMINHO_PROTO = __dirname + '/termoo.proto';
+
+const definicaoPacote = protoLoader.loadSync(
+  CAMINHO_PROTO,
+  {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true
+  }
+);
+
+const termoProto = grpc.loadPackageDefinition(definicaoPacote).termoo;
+
+async function main() {
+  const cliente = new termoProto.TermooService('localhost:50051', grpc.credentials.createInsecure());
+
+  // Promisificar os métodos
+  const novoJogo = util.promisify(cliente.NovoJogo.bind(cliente));
+  const enviarPalpite = util.promisify(cliente.EnviarPalpite.bind(cliente));
+
+  try {
+    const resposta = await novoJogo({});
+    const jogoId = resposta.jogo_id;
+    const tentativasMaximas = resposta.tentativas_maximas;
+    const tamanhoPalavra = resposta.tamanho_palavra;
+
+    console.log(`Tamanho da palavra: ${tamanhoPalavra}`);
+    console.log(`Tentativas máximas: ${tentativasMaximas}`);
+
+    let terminou = false;
+
+    while (!terminou) {
+      let palavra = readline.question('Digite sua palavra: ').trim();
+
+      const res = await enviarPalpite({ palavra, jogo_id: jogoId });
+      console.log('Resultado:', res.resultados);
+      console.log(res.mensagem);
+
+      terminou = res.terminou;
+    }
+
+  } catch (erro) {
+    console.error('Erro:', erro);
+  }
+}
+
+main();
---
+++ termoo/servidor.py
@@ -0,0 +1,97 @@
+import grpc
+from concurrent import futures
+import time
+import uuid
+import termoo_pb2
+import termoo_pb2_grpc
+import random
+
+PALAVRAS = ["carro", "festa", "garfo", "porta", "aureo", "teste", "testa", "corpo", "focos", "termo" ]
+
+
+class TermooServicer(termoo_pb2_grpc.TermooServiceServicer):
+    def __init__(self):
+        self.jogos = {}
+
+    def NovoJogo(self, request, context):
+        palavra = random.choice(PALAVRAS)
+        resposta_do_jogo = palavra
+        jogo_id = str(uuid.uuid4())
+        self.jogos[jogo_id] = {
+            "palavra": palavra,
+            "tentativas": 0,
+            "tentativas_maximas": 5,
+            "terminou": False
+        }
+        return termoo_pb2.NovoJogoResponse(
+            jogo_id=jogo_id,
+            tentativas_maximas=5,
+            tamanho_palavra=len(palavra)
+        )
+
+    def EnviarPalpite(self, request, context):
+        jogo = self.jogos.get(request.jogo_id)
+        if not jogo or jogo["terminou"]:
+            return termoo_pb2.PalpiteResponse(
+                resultados=[],
+                venceu=False,
+                terminou=True,
+                mensagem="Jogo inválido ou já finalizado"
+            )
+
+        palpite = request.palavra.lower()
+        alvo = jogo["palavra"]
+
+        if len(palpite) != len(alvo):
+            return termoo_pb2.PalpiteResponse(
+                resultados=[],
+                venceu=False,
+                terminou=False,
+                mensagem="Palavra com tamanho incorreto"
+            )
+
+        jogo["tentativas"] += 1
+
+        resultados = []
+        for i in range(len(palpite)):
+            if palpite[i] == alvo[i]:
+                resultados.append("correto")
+            elif palpite[i] in alvo:
+                resultados.append("posicao_errada")
+            else:
+                resultados.append("errado")
+
+        venceu = palpite == alvo
+        terminou = venceu or (jogo["tentativas"] >= jogo["tentativas_maximas"])
+        jogo["terminou"] = terminou
+        
+        if venceu:
+            mensagem = "Você venceu!"
+        elif terminou:
+            mensagem = f"Fim de jogo!"
+        else:
+            mensagem = "Tente novamente!"
+
+        return termoo_pb2.PalpiteResponse(
+            resultados=resultados,
+            venceu=venceu,
+            terminou=terminou,
+            mensagem=mensagem
+        )
+
+
+def servir():
+    servidor = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    termoo_pb2_grpc.add_TermooServiceServicer_to_server(TermooServicer(), servidor)
+    servidor.add_insecure_port('[::]:50051')
+    servidor.start()
+    print("Servidor gRPC rodando na porta 50051...")
+    try:
+        while True:
+            time.sleep(86400)
+    except KeyboardInterrupt:
+        servidor.stop(0)
+
+
+if __name__ == '__main__':
+    servir()
